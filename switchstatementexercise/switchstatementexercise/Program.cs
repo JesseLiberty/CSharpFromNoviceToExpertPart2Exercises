b917b55885aa4ae0b79d38af51f1695c7b3e1d6a
Jesse Liberty <jesseliberty@gmail.com>
2019-10-13 09:04:56 -0400

    switch revisited

---
+++ switchstatementexercise/switchstatementexercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace switchstatementexercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         Switch(42);
+         Switch("Life, the universe and everything");
+         Switch(3.33333);
+         Switch(false);
+      }
+
+      public void Switch(object param)
+      {
+         switch (param)
+         {
+            case int i when i > 100:
+               Console.WriteLine("you passed in a large integer");
+               break;
+            case int i when i >= 4:
+               Console.WriteLine("you passed in a medium integer");
+               break;
+            case int i when i < 4:
+               Console.WriteLine("you passed in a smaller integer");
+               break;
+            case double d:
+               Console.WriteLine("you passed in a double");
+               break;
+            case string s:
+               Console.WriteLine("you passed in a string");
+               break;
+            case bool b when !b:
+               Console.WriteLine("you passed in false");
+               break;
+            default:
+               Console.WriteLine("I don't know what you passed in ");
+               break;
+         }
+
+         ;
+      }
+   }
+}
