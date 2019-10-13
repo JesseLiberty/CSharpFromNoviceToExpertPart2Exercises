0df9f70aea7593c56e44c4939e48c6abaeb4e1bb
Jesse Liberty <jesseliberty@gmail.com>
2019-10-13 08:45:57 -0400

    null operators

---
+++ NullOperatorsExercise/NullOperatorsExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace NullOperatorsExercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         var puppy = new Dog
+         {
+            Name = "Charlie"
+         };
+         TellName(puppy);
+         puppy = null;
+         TellName(puppy);
+      }
+
+      public void TellName(Dog fido)
+      {
+         string name = fido?.Name ?? "Dog with no name";
+         Console.WriteLine(name);
+
+      }
+   }
+   public class Dog
+   {
+      public string Name { get; set; }
+   }
+}
