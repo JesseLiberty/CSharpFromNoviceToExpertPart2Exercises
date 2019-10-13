9eaa50dfc4b6819303c95633b9543553c6403a05
Jesse Liberty <jesseliberty@gmail.com>
2019-10-13 09:10:24 -0400

    async exercise

---
+++ asyncExercise/asyncExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asyncExercise
+{
+   class Program
+   {
+      static async Task Main(string[] args)
+      {
+         Console.WriteLine("1: Main started...");
+         await TakesALongTime();
+         Console.WriteLine("3: Main finished...");
+
+         Thread.Sleep(3000);
+      }
+
+      public static async Task TakesALongTime()
+      {
+         await Task.Delay(500);
+         Console.WriteLine("2: Finish TakesALongTime");
+      }
+   }
+}
