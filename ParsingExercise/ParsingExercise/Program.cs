413c0b2d1ccdc5e4889928e4bf35d8304da8f103
Jesse Liberty <jesseliberty@gmail.com>
2019-10-13 08:36:33 -0400

    answer parsing exercise

---
+++ ParsingExercise/ParsingExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParsingExercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var myString = "42";
+         var myConverter = int.TryParse(myString, out var meaningOfLife);
+         Console.WriteLine($"Converted to {meaningOfLife}");
+      }
+   }
+}
