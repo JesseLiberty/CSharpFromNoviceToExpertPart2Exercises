bdb9f8adaf7f42771fa8048d84758fd4182759f7
Jesse Liberty <jesseliberty@gmail.com>
2019-10-14 12:28:34 -0400

    Lambda

---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lambda
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Func<double, double, double> divider = (l, r) => l / r;
+
+         Console.WriteLine($"Dividing 5 / 7 {divider(5, 7)}");
+
+      }
+   }
+}
