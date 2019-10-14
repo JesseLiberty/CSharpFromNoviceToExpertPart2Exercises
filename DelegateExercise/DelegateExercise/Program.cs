1328f164f4caf0dcff50f056281d534b76b6191e
Jesse Liberty <jesseliberty@gmail.com>
2019-10-14 10:28:21 -0400

    Delegates Exercise

---
+++ DelegateExercise/DelegateExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegateExercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   public class Runner
+   {
+      public void Run()
+      {
+
+         var converter = new Converter();
+         Converts converts = converter.Convert;
+         Console.WriteLine($"{converts(5, "Jesse")}");
+      }
+   }
+
+   delegate string Converts(int left, string right);
+
+   public class Converter
+   {
+      public string Convert(int left, string right)
+      {
+         return left.ToString() + " " + right;
+      }
+
+   }
+}
