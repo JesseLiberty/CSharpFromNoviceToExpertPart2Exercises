1c5a1ebdf32f379dbebac5b91d1cb2a9f5f4303c
Jesse Liberty <jesseliberty@gmail.com>
2019-10-15 12:17:20 -0400

    LINQ Operators

---
+++ LinqOperatorsExercise/LinqOperatorsExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqOperatorsExercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var numberList = new List<int>
+         {
+            1, 2, 1, 3, 3, 4, 3, 6, 2, 7, 3, 4, 12, 9
+         };
+
+         var numbers = numberList
+            .Distinct()
+            .Skip(2)
+            .Take(5)
+            .Select(x => x);
+
+         foreach (var number in numbers)
+         {
+            Console.Write(number + " ");
+         }
+
+
+      }
+   }
+}
