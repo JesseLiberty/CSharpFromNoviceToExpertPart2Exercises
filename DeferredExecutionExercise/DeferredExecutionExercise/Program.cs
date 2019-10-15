c8a48e021af74152edcd5eadaa484e9ebde1c1b3
Jesse Liberty <jesseliberty@gmail.com>
2019-10-15 11:45:59 -0400

    Deferred Execution

---
+++ DeferredExecutionExercise/DeferredExecutionExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace DeferredExecutionExercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Counter();
+      }
+
+      public static void Counter()
+      {
+         var query = Enumerable.Range(0, 1000000)
+            .Select(x =>
+            {
+               Thread.Sleep(1000);
+               return x;
+            });
+
+         foreach (var num in query)
+         {
+            Console.WriteLine(num);
+         }
+      }
+   }
+}
