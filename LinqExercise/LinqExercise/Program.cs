24d23123ae2dbcc949f61a1276d6eb4db467edd1
Jesse Liberty <jesseliberty@gmail.com>
2019-10-15 10:10:22 -0400

    LinqExercise

---
+++ LinqExercise/LinqExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+   }
+
+   class Runner
+   {
+      public void Run()
+      {
+         var query = from method in typeof(string).GetMethods()
+            orderby method.Name
+            group method by method.Name
+            into groups
+            select new
+               {MethodName = groups.Key, MethodOverloads = groups.Count()};
+
+         foreach (var item in query)
+         {
+            Console.WriteLine(item);
+         }
+      }
+   }
+}
