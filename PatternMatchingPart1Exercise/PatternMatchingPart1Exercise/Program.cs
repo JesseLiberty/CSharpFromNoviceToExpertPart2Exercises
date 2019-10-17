f9c3fa2f39d87c843e16e9a4f36d3b9922f5adfd
Jesse Liberty <jesseliberty@gmail.com>
2019-10-17 08:52:59 -0400

    Pattern Matching Part 1 Exercise

---
+++ PatternMatchingPart1Exercise/PatternMatchingPart1Exercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PatternMatchingPart1Exercise
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var runner = new Runner();
+         runner.Run();
+      }
+
+      public class Runner
+      {
+         public void Run()
+         {
+            var fineComputer = new FineComputer();
+            var zero = fineComputer.ComputeFine(0);
+            Console.WriteLine($"The fine for having no passengers is ${zero}");
+         }
+      }
+
+      public class FineComputer
+      {
+         public double ComputeFine(int? numPassengers) =>
+            numPassengers switch
+            {
+               0 => 100.00,
+               1 => 40.00,
+               2 => 20.00,
+               3 => 0.0,
+               { } => throw new ArgumentException(message: "Unknown number"),
+               null => throw new ArgumentNullException()
+            };
+      }
+   }
+}
